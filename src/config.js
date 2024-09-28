633474269516db4dd2c4ca0c349a75295b5ad188
Antonio Vildes Barbosa <avildes@gmail.com>
2024-09-28 10:45:56 -0300

    feat: adding worker cname

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 //export const API_URL = "https://api.scryfall.com/cards/random?q=type%3Agoblin has%3Aflavor&pretty=1";
-export const API_URL = "https://goblinwisdom.avildes.workers.dev";
+//export const API_URL = "https://goblinwisdom.avildes.workers.dev";
+export const API_URL = "worker.goblinwisdom.com";
