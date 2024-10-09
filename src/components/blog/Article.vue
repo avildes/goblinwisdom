4850f33c2f6b7904de56aa383450b6a20c611395
Antonio Vildes Barbosa <antonio.vildes@bairesdev.com>
2024-10-09 18:17:34 -0300

    fix: testing vite

--- src/components/blog/Article.vue
+++ src/components/blog/Article.vue
@@ -7,7 +7,7 @@
 <script setup>
 import { ref, computed } from "vue";
 import { marked } from "marked";
-const mdfile = "@/assets/markdown/Symlinks.md";
+import mdfile from "@/assets/markdown/Symlinks.md?url";
 const markdown = ref("");
 
 const getMarkdownData =
